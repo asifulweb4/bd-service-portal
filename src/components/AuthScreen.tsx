fde96f9de75a6a7ca2778e2d9da32cf93e911900
asifulweb4 <asifulweb4@gmail.com>
2026-06-10 13:48:30 +0600

    logo update1

--- src/components/AuthScreen.tsx
+++ src/components/AuthScreen.tsx
@@ -2,7 +2,8 @@ import React, { useState } from 'react';
 import { localAuth } from '../lib/localAuth';
 import { Sparkles, ShieldCheck, Mail, Lock, User, LogIn, UserPlus, AlertCircle, RefreshCw, Phone, ArrowLeft, CheckCircle, Eye, EyeOff } from 'lucide-react';
 
-const brandLogo = "../src/assets/images/bdlogo.png";
+// ফাইলটি সরাসরি ইমপোর্ট করুন
+import brandLogo from '../assets/images/bdlogo.png';
 
 interface AuthScreenProps {
   onAuthSuccess: () => void;
